7238efc3c468c02d5ba34a8f3a21208fa360ad51
Andrew Pachuilo <Andrew Pachuilo>
2016-11-27 18:10:49 -0600

    Altered cluster browser

--- src/components/ClusterBrowser.js
+++ src/components/ClusterBrowser.js
@@ -1,5 +1,4 @@
 import React, { PropTypes } from 'react'
-import debounce from 'lodash.debounce'
 
 export class ClusterBrowser extends React.Component {
   constructor (props) {
@@ -11,68 +10,49 @@ export class ClusterBrowser extends React.Component {
     }
 
     this.onClick = this.onClick.bind(this)
-    this.handleResize = debounce(this.handleResize.bind(this), 500)
-  }
 
-  componentDidMount () {
-    window.addEventListener('resize', this.handleResize, false)
-    this.handleResize()
-  }
-
-  componentWillUnmount () {
-    window.removeEventListener('resize', this.handleResize)
-  }
-
-  handleResize () {
-    let rootBBOX = this.refs.root.getBoundingClientRect()
-    let offset = document.body.scrollTop
-    this.setState({
-      width: rootBBOX.width,
-      height: document.body.clientHeight - (rootBBOX.top + offset) - 20
+    this.clusters = new Map()
+    this.props.cocktails.forEach((d, i) => {
+      if (this.clusters.has(d.cluster)) {
+        this.clusters.get(d.cluster).push(d)
+      } else {
+        this.clusters.set(d.cluster, [])
+        this.clusters.get(d.cluster).push(d)
+      }
     })
+
+    this.clusters = new Map([...this.clusters.entries()].sort((a, b) => {
+      return b[1].length - a[1].length
+    }))
   }
 
   onClick (e) {
-    let i = e.target.getAttribute('data-index')
-    let d = this.props.cocktails[i]
+    let iString = e.currentTarget.dataset.index
+    let indicies = iString.split(',').map((i) => +i)
+    let d = this.clusters.get(indicies[0])[indicies[1]]
     this.props.onClick(d)
   }
 
   render () {
-    let { margins } = this.props
-    let { width, height } = this.state
-    let svgWidth = width + margins.left + margins.right
-    let svgHeight = height + margins.top + margins.bottom
     return (
-      <div ref='root'>
-        <svg width={svgWidth} height={svgHeight}>
-          {this.props.cocktails.map((d, i) => {
-            let x = 0
-            let y = 0
-            if (('x' in d) && ('y' in d)) {
-              x = d.x
-              y = d.y
-            } else {
-              x = d.x = (Math.floor(Math.random() * width) + 1) / width
-              y = d.y = (Math.floor(Math.random() * height) + 1) / height
-            }
-            let position = 'translate(' + (x * width) + ',' + (y * height) + ')'
-            let clusterClass = 'cluster-' + d.cluster
-            if (this.props.suggestedCocktails.includes(d)) {
-              clusterClass += ' highlighted'
-            }
-            return (
-              <g key={i} className='clusterPoint' transform={position}>
-                <circle
-                  r={5}
-                  data-index={i}
-                  className={clusterClass}
-                  onClick={this.onClick} />
-                <text dy={20}>{d.title}</text>
-              </g>
-            )
-          })}
-        </svg>
+      <div className='row'>
+        {Array.from(this.clusters).map((cluster, index) => {
+          let clusterClass = 'row cluster-' + cluster[0]
+          return (
+            <div key={index} className={clusterClass}>
+              {cluster[1].map((d, i) => {
+                let itemClass = 'clusterItem'
+                if (this.props.suggestedCocktails.includes(d)) itemClass += ' highlighted'
+                return (
+                  <div key={i} className={itemClass} data-index={cluster[0] + ',' + i} onClick={this.onClick}>
+                    <img src={d.picture} />
+                    <span>{d.title}</span>
+                  </div>
+                )
+              })}
+            </div>
+          )
+        })}
       </div>
     )
   }
@@ -80,14 +60,12 @@ export class ClusterBrowser extends React.Component {
 
 ClusterBrowser.defaultProps = {
   onClick: () => {},
-  margins: { top: 20, right: 20, bottom: 20, left: 20 },
   cocktails: [],
   suggestedCocktails: []
 }
 
 ClusterBrowser.propTypes = {
   onClick: PropTypes.func,
-  margins: PropTypes.object,
   cocktails: PropTypes.array,
   suggestedCocktails: PropTypes.array
 }
