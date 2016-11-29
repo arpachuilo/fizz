9a3aa42a95cbf677e210d94e6a8082d2959eeb72
arpachuilo <andrew.pachuilo@gmail.com>
2016-11-28 23:10:17 -0600

    Revert "More styling changes"
    This reverts commit b0691e2e015e7ee0a2ddbe20aaf4cb63955f9eb1.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -19,7 +19,7 @@ class Home extends React.Component {
 
   render () {
     return (
-      <div className='container'>
+      <div className='row'>
         <CocktailGrid items={this.props.cocktails} onButtonClick={this.removeCocktail} />
         {
           this.props.cocktails.length < 1
